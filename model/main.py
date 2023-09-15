189e8c18d10ae886234f33dafca59d205bd43c57
hp <engrziaurrehman.kicsit@gmail.com>
2023-09-15 10:13:43 +0500

    update

--- model/main.py
+++ model/main.py
@@ -1,28 +1,28 @@
 import pandas as pd
 from sklearn.metrics import accuracy_score, classification_report
-# from sklearn import datasets
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.model_selection import train_test_split
-# import pickle as pk
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import pickle as pk
 
-# def createModel(iris):
-#     x = iris.data
-#     y = iris.target
+def createModel(iris):
+    x = iris.data
+    y = iris.target
 
-#     # split the data
-#     xTrain, xTest, yTrain, yTest = train_test_split(x, y, 
-#                                                     test_size=0.2, random_state=42)
-#     # train
-#     model = RandomForestClassifier()
-#     model.fit(xTrain, yTrain)
+    # split the data
+    xTrain, xTest, yTrain, yTest = train_test_split(x, y, 
+                                                    test_size=0.2, random_state=42)
+    # train
+    model = RandomForestClassifier()
+    model.fit(xTrain, yTrain)
 
-#     # test the model
-#     yPred = model.predict(xTest)
+    # test the model
+    yPred = model.predict(xTest)
 
-#     print("Accuracy:", accuracy_score(yTest, yPred))
-#     print("Classification Report: \n", classification_report(yTest, yPred))
+    print("Accuracy:", accuracy_score(yTest, yPred))
+    print("Classification Report: \n", classification_report(yTest, yPred))
 
-#     return model
+    return model
 
 
 
